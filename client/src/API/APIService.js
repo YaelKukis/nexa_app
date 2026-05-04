4990a6e3b48437e5081a23e9e8653ef724f8cd07
YaelKukis <yael3223717@gmail.com>
2026-05-04 17:35:53 +0300

    update API base URL to production

--- client/src/API/APIService.js
+++ client/src/API/APIService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://nexa-app-server.onrender.com',
     headers: {
         'Content-Type': 'application/json',
     },
