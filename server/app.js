6f056394df71dea56b99558c62fed1742fdfee86
YaelKukis <yael3223717@gmail.com>
2026-05-04 20:02:57 +0300

    revert CORS to open

--- server/app.js
+++ server/app.js
@@ -9,9 +9,7 @@ const photoRoutes = require("./routes/photoRoutes");
 
 const app = express();
 
-app.use(cors({
-  origin: 'https://shimmering-vacherin-2b0a04.netlify.app'
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRoutes);
